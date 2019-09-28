36578325638a36d79383fc6c127de019db6ce5fa
Marcin Ulman <marcin@ulman@dev>
2019-09-28 16:17:06 +0200

    setup service

--- src/main/java/dev/ulman/dmsspringmvc/DmsSpringMvcApplication.java
+++ src/main/java/dev/ulman/dmsspringmvc/DmsSpringMvcApplication.java
@@ -2,7 +2,11 @@ package dev.ulman.dmsspringmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class DmsSpringMvcApplication {
 
@@ -10,4 +14,8 @@ public class DmsSpringMvcApplication {
 		SpringApplication.run(DmsSpringMvcApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
