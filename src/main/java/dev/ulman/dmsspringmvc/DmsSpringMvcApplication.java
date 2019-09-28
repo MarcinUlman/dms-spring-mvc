ffdba09a35f4201cb48ea50996d795bd7acef7f4
Marcin Ulman <marcin@ulman@dev>
2019-09-28 09:45:39 +0200

    init spring-mvc

---
+++ src/main/java/dev/ulman/dmsspringmvc/DmsSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package dev.ulman.dmsspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ulman/dmsspringmvc/DmsSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.ulman.dmsspringmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmsSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
